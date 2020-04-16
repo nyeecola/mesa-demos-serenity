nyeecola/mesa-demos-serenity

commit 7fa3a4335154753973ee7f0d1efd5e40a93e454d

wglgears: mark application as dpi-aware
This makes rendering nice and crisp on high-dpi systems. Since we're not
doing any layouting or anything, we can just claim to be aware without
doing much.

We keep the current window-size when the DPI changes, because the size
is specified in pixels, so it seems more useful to keep that as-is.

Reviewed-by: Brian Paul <brianp@vmware.com>

src/wgl/wglgears.c
@@ -341,6 +341,11 @@ WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
       else if (wParam == VK_ESCAPE)
          PostQuitMessage(0);
       return 0;
+#if WINVER >= 0x0605
+   case WM_NCCREATE:
+       EnableNonClientDpiScaling(hWnd);
+       break;
+#endif
    }
 
    return DefWindowProc(hWnd, uMsg, wParam, lParam);
@@ -375,6 +380,10 @@ make_window(const char *name, int x, int y, int width, int height)
       0,
       0, 0, 0
    };
+
+#if WINVER >= 0x0605
+   SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+#endif
 
    winrect.left = (long)0;
    winrect.right = (long)width;

commit d24d852fc72b7b61fbae0599ab495cd2ae7bcfbe

wglgears: set whole process as dpi-aware
It seems different GPU drivers behave differently in the case of
per-thread DPI-awareness, causing incorrect rendering on drivers like
the NVIDIA-driver. This is probably related to some internal threading
in the drivers, and is probably a subtle driver-bug.

In either case, setting per-process DPI awareness seems to do the trick.
It doesn't matter to us if we're doing per process or per thread, so
let's just go with the more robust solution.

Fixes: 7fa3a433 ("wglgears: mark application as dpi-aware")

src/wgl/wglgears.c
@@ -382,7 +382,7 @@ make_window(const char *name, int x, int y, int width, int height)
    };
 
 #if WINVER >= 0x0605
-   SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
+   SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE);
 #endif
 
    winrect.left = (long)0;